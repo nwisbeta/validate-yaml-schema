56e16b722fb2afa9ce9d1e989be9e682e87cb9e5
Huw Angle <huw.angle@gmail.com>
2020-02-16 17:04:34 +0000

    add webpack and create bundle

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  mode : 'development',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'lib'),
+  },
+  module: {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        },
+        {
+            test:/node_modules[\\|/](yaml-language-server|vscode-languageserver|prettier)/,
+            use: 'umd-compat-loader'
+        }
+    ]
+},
+target: 'node',
+resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+}
+}
+
+
